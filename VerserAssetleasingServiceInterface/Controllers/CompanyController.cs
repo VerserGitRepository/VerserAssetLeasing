99584133c25546f04d622971be9fa4ec21a20d04
Ragini <ragini.malhotra@verser.com.au>
2020-01-14 17:07:05 +1100

    Company details UI changes

--- VerserAssetleasingServiceInterface/Controllers/CompanyController.cs
+++ VerserAssetleasingServiceInterface/Controllers/CompanyController.cs
@@ -1,8 +1,11 @@
 ﻿using System;
 using System.Collections.Generic;
+using System.IO;
 using System.Linq;
 using System.Web;
 using System.Web.Mvc;
+using System.Web.UI;
+using System.Web.UI.WebControls;
 using VerserAssetleasingServiceInterface.Models;
 using VerserAssetleasingServiceInterface.ServiceImplentationhelper;
 
@@ -20,5 +23,33 @@ namespace VerserAssetleasingServiceInterface.Controllers
             return View(companydata);
         }
 
+        [HttpPost]
+        public ActionResult ExportTimesSheetToExcel()
+        {
+            List<CompanyListViewModel> Companymodel = new List<CompanyListViewModel>();
+            if (Session["Username"] == null)
+            {
+                return RedirectToAction("Login", "Login");
+            }
+            else
+            {
+                Companymodel = CompanyServicehelper.Projects().Result;
+                GridView gv = new GridView();
+                gv.DataSource = Companymodel;
+                gv.DataBind();
+                Response.ClearContent();
+                Response.Buffer = true;
+                Response.AddHeader("content-disposition", "attachment; filename=CompanyList.xls");
+                Response.ContentType = "application/ms-excel";
+                Response.Charset = "";
+                StringWriter sw = new StringWriter();
+                HtmlTextWriter htw = new System.Web.UI.HtmlTextWriter(sw);
+                gv.RenderControl(htw);
+                Response.Output.Write(sw.ToString());
+                Response.Flush();
+                Response.End();
+            }
+            return RedirectToAction("Index", "Company");
+        }
     }
 }
