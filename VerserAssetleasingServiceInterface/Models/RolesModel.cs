421bdcc8ce755f12d6584da3425c7c798adf1f01
Verser <verser@outlook.com.au>
2020-01-14 17:03:53 +1100

    Latest Changes

---
+++ VerserAssetleasingServiceInterface/Models/RolesModel.cs
@@ -0,0 +1,13 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+
+namespace VerserAssetleasingServiceInterface.Models
+{
+    public class RolesModel
+    {
+        public string RoleName { get; set; }
+        public string LoweredRoleName { get; set; }
+    }
+}
