e2b1dc62e5675ef162cc93e03f50310e42d94c42
Verser <verser@outlook.com.au>
2020-08-05 09:18:39 +1000

    chnages

--- VerserAssetleasingServiceInterface/Models/PostQuoteRequestModel.cs
+++ VerserAssetleasingServiceInterface/Models/PostQuoteRequestModel.cs
@@ -1,5 +1,6 @@
 ﻿using System;
 using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
 using System.Linq;
 using System.Web;
 
@@ -11,7 +12,9 @@ namespace VerserAssetleasingServiceInterface.Models
         public int OpportunityNumber { get; set; }
         public string OpportunityName { get; set; }
         public string Email { get; set; }
-        public string CustomerName { get; set; }        
+        public string CustomerName { get; set; }
+        [DataType(DataType.Date)]
+        [DisplayFormat(ApplyFormatInEditMode = true, DataFormatString = "{0:MM-dd-yyyy}")]
         public DateTime StartDate { get; set; }
         public string CustomerContactName { get; set; }
         public string SalesforceOpportunityName { get; set; }
